93c3a694769d56b006060c36e76d13d8c88120e4
Basir98 <basirram98@gmail.com>
2022-04-30 22:05:37 +0200

    user sign up page and route is finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_full_title(page_title = "")
+    base_title = "Post App" 
+    if page_title.empty?
+        base_title
+    else
+        page_title + " | " + base_title
+    end
+  end
 end
